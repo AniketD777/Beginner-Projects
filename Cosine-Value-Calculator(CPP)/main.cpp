b80ab8910b608f03817471af1512d4246bffcac0
Aniket Das <145093302+AniketD777@users.noreply.github.com>
2023-09-15 13:34:37 +0530

    Create main.cpp

---
+++ Cosine-Value-Calculator(CPP)/main.cpp
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<math.h>
+
+int fac(int n)
+{
+	if(n==0 || n==1)
+	{
+		return 1;
+	}
+	return n*fac(n-1);
+}
+
+int main()
+{
+	float ang,rad;
+	printf("Enter degrees to calculate cosine value: ");
+	scanf("%f",&ang);
+	//If angle greater than 360 degrees
+	while(ang>=360)
+	{
+		ang=ang-360;
+	}
+	int sign=0; //0=>+ve && 1=>-ve
+	float finAng;
+	//Now to set sign for different quadrants 
+	if(ang>270)
+	{
+		finAng=360-ang;
+	}
+	else if(ang>180)
+	{
+		finAng=ang-180;
+		sign=1;
+	}
+	else if(ang>90)
+	{
+		finAng=180-ang;
+		sign=1;
+	}
+	else
+	{
+		finAng=ang;
+	}
+	rad=(finAng/180)*3.14159265;
+	float Cos=0;
+	int check=0; //If '0' then add and if '1' then subtract
+	float x=rad;
+	/*We can take no. of expressions('n') by user also but lets consider 15
+      printf("Enter n: ");
+	  scanf("%d",&n);*/
+    int n=15;
+	for(int i=0;i<n;i+=2)
+	{
+		if(check==0)
+		{
+			Cos+=pow(x,i)/fac(i);
+			check=1;
+		}
+		else
+		{
+			Cos-=pow(x,i)/fac(i);
+			check=0;
+		}
+	}
+	if(sign==0)
+	{
+		printf("By Series Expansion: %f\n",Cos);
+	}
+	else
+	{
+	    printf("By Series Expansion: -%f\n",Cos);
+	}
+	rad=(ang/180)*3.14159265; //Using the input ang directly as radians
+	printf("By inbuilt function using <math.h> lib: %f\n",cos(rad));
+	return 0;
+}
